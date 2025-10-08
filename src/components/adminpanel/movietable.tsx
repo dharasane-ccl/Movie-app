5fb6464f266fb893e7e7b502879cf7d4561ecb3e
dharasane-ccl <dharsana.e@crystalcodelabs.com>
2025-10-08 11:27:56 +0530

    updated movietable

--- src/components/adminpanel/movietable.tsx
+++ src/components/adminpanel/movietable.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Movie } from './types';
-import { Button, Pagination } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 interface MovieTableProps {
@@ -8,23 +8,45 @@ interface MovieTableProps {
   onEdit: (movie: Movie) => void;
   onDelete: (id: string) => void;
   onView: (movie: Movie) => void;
-  
+
 }
-
 const MovieTable: React.FC<MovieTableProps> = ({ movies, onEdit, onView, onDelete }) => {
- 
-  const itemsPerPage = 4;
+
+  const [itemsPerPage, setItemsPerPage] = useState(6)
   const [currentPage, setCurrentPage] = useState(1);
+  const [filterterm, setFiltreTerm] = useState('')
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = movies.slice(indexOfFirstItem, indexOfLastItem);
+  const filteredMovies = useMemo(() => {
+    return movies.filter(movie =>
+      movie.title.toLowerCase().includes(filterterm.toLowerCase())
+    );
+  }, [movies]);
 
-  const totalPages = Math.ceil(movies.length / itemsPerPage);
+  const totalPages = useMemo(() => {
+    return Math.ceil(filteredMovies.length / itemsPerPage);
+  }, [filteredMovies, itemsPerPage]);
 
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
+  const paginatedMovies = useMemo(() => {
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    return filteredMovies.slice(indexOfFirstItem, indexOfLastItem);
+  }, [filteredMovies, currentPage, itemsPerPage]);
+
+  const paginationStatus = useMemo(() => {
+    const totalFilteredItems = filteredMovies.length;
+    if (totalFilteredItems === 0) {
+      return "0-0 of 0";
+    }
+    const indexOfFirstItem = (currentPage - 1) * itemsPerPage + 1;
+    const indexOfLastItem = Math.min(currentPage * itemsPerPage, totalFilteredItems);
+
+    return `${indexOfFirstItem}-${indexOfLastItem} of ${totalFilteredItems}`;
+  }, [filteredMovies, currentPage, itemsPerPage]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [filterterm, itemsPerPage]);
+
   return (
     <div className="table-responsive">
       <table className="table table-striped table-hover">
@@ -41,7 +63,7 @@ const MovieTable: React.FC<MovieTableProps> = ({ movies, onEdit, onView, onDelet
           </tr>
         </thead>
         <tbody>
-          {currentItems.map((movie, index) => (
+          {paginatedMovies.map((movie: Movie, index: number) => (
             <tr key={movie._id}>
               <td>{(currentPage - 1) * itemsPerPage + index + 1}</td>
               <td>{movie.title}</td>
@@ -59,7 +81,7 @@ const MovieTable: React.FC<MovieTableProps> = ({ movies, onEdit, onView, onDelet
               <td>
                 {movie.targetUrl ? (
                   <a href={movie.targetUrl} target="_blank" rel="noopener noreferrer" className='d-flex'>
-                 watch 
+                    watch
                   </a>
                 ) : (
                   'No URL'
@@ -67,31 +89,37 @@ const MovieTable: React.FC<MovieTableProps> = ({ movies, onEdit, onView, onDelet
               </td>
               <td>
                 <div className='d-flex '>
-                <Button variant="outline-white" size="lg" className="me-2" onClick={() => onEdit(movie)}>
-                  <i className="bi bi-pencil"></i>
-                </Button>
-                <Button variant="outline-white" size="lg" className="me-2" onClick={() => onView(movie)}>
-                  <i className="bi bi-eye"></i>
-                </Button>
-                <Button variant="outline-white" size="lg" className="me-2 " onClick={() => onDelete(movie._id)}>
-                  <i className="bi bi-trash"></i>
-                </Button>
+                  <Button variant="outline-white" size="lg" className="me-2" onClick={() => onEdit(movie)}>
+                    <i className="bi bi-pencil"></i>
+                  </Button>
+                  <Button variant="outline-white" size="lg" className="me-2" onClick={() => onView(movie)}>
+                    <i className="bi bi-eye"></i>
+                  </Button>
+                  <Button variant="outline-white" size="lg" className="me-2 " onClick={() => onDelete(movie._id)}>
+                    <i className="bi bi-trash"></i>
+                  </Button>
                 </div>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <div className="d-flex justify-content-center mt-3">
-        <Pagination>
-          <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
-          {Array.from({ length: totalPages }, (_, i) => (
-            <Pagination.Item key={i + 1} active={i + 1 === currentPage} onClick={() => handlePageChange(i + 1)}>
-              {i + 1}
-            </Pagination.Item>
-          ))}
-          <Pagination.Next onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} />
-        </Pagination>
+      <div className="d-flex justify-content-end">
+
+        <Form.Select
+          value={itemsPerPage}
+          onChange={(e) => setItemsPerPage(Number(e.target.value))}
+          style={{ width: '67px' }}
+        >
+          <option value="4">4 </option>
+          <option value="6">6 </option>
+          <option value="10">10 </option>
+          <option value="18">18</option>
+        </Form.Select>
+
+        <span className="text-muted my-2 mx-5">
+          {paginationStatus}
+        </span>
       </div>
     </div>
   );
